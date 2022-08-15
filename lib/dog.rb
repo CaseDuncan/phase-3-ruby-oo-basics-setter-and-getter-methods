e7ae91bb0fb43903543dfb80a99e9a597db19efa
CaseDuncan <oluochdancun19@gmail.com>
2022-08-15 14:28:40 +0300

    setters and getters

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
 
+
+    #setter
+    def name=(name)
+        @name = name
+    end
+
+    #getter
+    def name
+        @name
+        
+    end
+
+    #breed setter
+    def breed=(breed)
+        @breed = breed
+        
+    end
+
+    #breed getter
+    def breed
+        @breed
+        
+    end
+
 end
